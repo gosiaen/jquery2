dd57ccbcfba47302c5347915df14f173325678ec
Gosia <emeneczka@gmail.com>
2017-05-17 17:54:50 +0200

    all

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
 $(function(){
 	console.log('DOM załadowany');
+	
 	var carouselList = $('#carousel ul');
 	setInterval(changeSlide, 3000); 
+	
 	function changeSlide(){
-		carouselList.animate({'marginLeft':-400}), 400, moveFirstSlide; 
+		carouselList.animate({'marginLeft':-400}, 400, moveFirstSlide);
+	};
+	
 	function moveFirstSlide(){
 		var firstItem = carouselList.find('li:first');
 		var lastItem = carouselList.find('li:last');
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
 	};
-};
 });
